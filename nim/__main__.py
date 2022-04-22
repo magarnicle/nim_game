2e1ffc1b7af4a7dac4e730a034c7f15687da686e
Matt Rademaker <matthew@acctv.com.au>
2022-04-22 21:17:01 +1000

    convenience improvements e.g. automatically take last object

--- nim/__main__.py
+++ nim/__main__.py
@@ -1,7 +1,7 @@
 """Nim game.
 
 Usage:
-    nim_game [-cm] STACK ...
+    nim [-cm] STACK ...
 
 Options:
     STACK                   One or more stacks, being the number of objects in the stack.
@@ -12,9 +12,11 @@ import sys
 import typing as ty
 from enum import IntEnum
 from dpcontracts import ensure, PostconditionError
+import random
 
 from docopt import docopt
 from pprint import pprint
+
 
 class PlayerType(IntEnum):
     """Whether a player is a computer or a human."""
@@ -31,17 +33,21 @@ class PlayerType(IntEnum):
     def __sub__(self, value):
         return self.__add__(-value)
 
+
 def take_objects(stack: int, objects_to_take: int) -> None:
     """Take a number of objects from a stack."""
     return max(0, stack - objects_to_take)
+
 
 def stack_empty(stack: int) -> bool:
     """Check if a stack is empty."""
     return stack == 0
 
+
 def game_over(stacks: ty.List[int]) -> bool:
     """Check if the game is over."""
     return all(map(stack_empty, stacks))
+
 
 def _empty_stack_check(args, ret):
     return not list(map(stack_empty, args.stacks))[ret[0]]
@@ -49,53 +55,74 @@ def _empty_stack_check(args, ret):
 def _positive_object_count_check(args, ret):
     return ret[1] > 0
 
+
 @ensure("Number of objects must be greater than 0", _positive_object_count_check)
 @ensure("Cannot remove object from empty stack", _empty_stack_check)
 def get_move(player_type: PlayerType, stacks: ty.List[int]) -> ty.Tuple[int, int]:
     """Get a new move (a stack index and a number objects to take).
     The move can come from a human or the computer."""
-    match player_type:
-        case PlayerType.COMPUTER:
-            #return _get_computer_move(stacks)
-            return _get_human_move(stacks)
-        case PlayerType.HUMAN:
-            return _get_human_move(stacks)
+    if player_type == PlayerType.COMPUTER:
+        return _get_computer_move(stacks)
+    return _get_human_move(stacks)
 
 
 def _get_computer_move(stacks: ty.List[int]) -> ty.Tuple[int, int]:
-    return 0, 0
+    """Get a move from the computer."""
+    valid_stacks = get_valid_stacks(stacks)
+    stack = valid_stacks[random.randint(0, len(valid_stacks) - 1)]
+    return stack, random.randint(1, stacks[stack])
+
 
 def _get_human_move(stacks: ty.List[int]) -> ty.Tuple[int, int]:
-    return int(input("Which stack? ")), int(input("How many objects to take? "))
+    """Ask the human for a move."""
+    valid_stacks = get_valid_stacks(stacks)
+    return (
+        int(input("Which stack? ")) if len(valid_stacks) > 1 else valid_stacks[0],
+        int(input("How many objects to take? ")),
+    )
 
-def determine_winner(current_player: PlayerType, misere: bool=False) -> PlayerType:
+
+def get_valid_stacks(stacks: ty.List[int]) -> ty.List[int]:
+    """Get the indices of non-empty stacks."""
+    return [i for i, stack in enumerate(stacks) if not stack_empty(stack)]
+
+
+def determine_winner(current_player: PlayerType, misere: bool = False) -> PlayerType:
     if misere:
         return current_player
     return current_player + 1
 
-def print_game_board(stacks: ty.List[int], player_turn: PlayerType) -> None:
+
+def print_game_board(stacks: ty.List[int]) -> None:
     """Print the state of the game."""
     index_texts = ""
     object_texts = ""
     for index, stack in enumerate(stacks):
         object_text = str(stack)
         object_texts += f"\t{object_text}"
-        index_text = (str(index) + (" " * len(object_text)))[:len(object_text)]
+        index_text = (str(index) + (" " * len(object_text)))[: len(object_text)]
         index_texts += f"\t{index_text}"
-    index_texts = index_texts.lstrip('\t')
-    object_texts = object_texts.lstrip('\t')
+    index_texts = index_texts.lstrip("\t")
+    object_texts = object_texts.lstrip("\t")
     print("\n\tStacks\t")
     print(f"Index:   {index_texts}")
     print(f"Objects: {object_texts}")
-    print(f"It is {player_turn.name}'s turn.")
+
 
 def main(ops: ty.Dict[str, ty.Any]) -> int:
     pprint(ops, stream=sys.stderr)
     stacks = [int(stack) for stack in ops["STACK"]]
-    assert all(map(lambda stack: stack > 0, stacks)), "Stacks must have at least one object in them"
+    assert all(
+        map(lambda stack: stack > 0, stacks)
+    ), "Stacks must have at least one object in them"
     player_turn = PlayerType.COMPUTER if ops["--computer-first"] else PlayerType.HUMAN
     while not game_over(stacks):
-        print_game_board(stacks, player_turn)
+        print_game_board(stacks)
+        print(f"It is {player_turn.name}'s turn.")
+        if sum(stacks) == 1:
+            # One left, take it and end the game
+            player_turn += 1
+            break
         while True:
             try:
                 stack_index, objects_to_take = get_move(player_turn, stacks)
@@ -106,9 +133,10 @@ def main(ops: ty.Dict[str, ty.Any]) -> int:
                 break
         stacks[stack_index] = take_objects(stacks[stack_index], objects_to_take)
         player_turn += 1
+    print(f"{(player_turn - 1).name} takes the last object...")
     print(f"{determine_winner(player_turn, ops['--misère']).name} won!")
     return 0
 
-if __name__ == '__main__':
-    sys.exit(main(docopt(__doc__)))
 
+if __name__ == "__main__":
+    sys.exit(main(docopt(__doc__)))
