a4bdab416002771f76580f9457e4fcbfd20890f9
Matt Rademaker <matthew@acctv.com.au>
2022-08-18 17:03:30 +1000

    logging and formatting of stacks

--- game.js
+++ game.js
@@ -1,39 +1,23 @@
+var game_board = document.getElementById("game_board");
+var game_log = document.getElementById("game_log");
 var players = [
     {"name": "Hugh", "type": "Human"},
     {"name": "Rob", "type": "Robot"}
 ];
 var game_state = {
     "player_turn": 0,
-    "stacks": [9, 9, 9]
+    "stacks": [9, 9, 9],
+    "misere": false
 };
 
-log(game_state);
 function log(message){
     console.log(message);
+    game_log.innerHTML = game_log.innerHTML + "<br>" + message;
 };
 
-function create_game_board(){
-    var game_board = document.getElementById("game_board");
-    log(game_board)
-    var all_stacks_display = '';
-    log(game_state);
-    for (stack in game_state["stacks"]){
-        log("stack: " + stack);
-        stack_display = '    <div id="stack_' + stack + '">\n'
-        for (var j=0; j<game_state["stacks"][stack]; j++){
-            log("j: " + j);
-            stack_display = stack_display + '      <div id="coin_' + j + '" onmouseup="take_turn(' + stack + ', ' + j + ')">' + j + '</div>\n';
-        };
-        stack_display = stack_display + '    </div>\n';
-        log(stack_display);
-        all_stacks_display = all_stacks_display + stack_display;
-    };
-    log(all_stacks_display);
-    game_board.innerHTML = all_stacks_display;
-};
 
 function game_over(){
-    for (stack in game_state["stacks"]){
+    for (stack of game_state["stacks"]){
         if (stack > 0){
             return false;
         };
@@ -42,11 +26,16 @@ function game_over(){
 };
 
 function update_game_board(){
-    return true;
-};
-
-function get_winner(){
-    return players[0];
+    var all_stacks_display = '';
+    for (stack in game_state["stacks"]){
+        stack_display = '    <tr id="stack_' + stack + '">\n      <td>Stack ' + stack + ":</td>\n";
+        for (var j=0; j<game_state["stacks"][stack]; j++){
+            stack_display = stack_display + '      <td id="coin_' + j + '" onmouseup="take_turn(' + stack + ', ' + j + ')">' + j + '</td>\n';
+        };
+        stack_display = stack_display + '    </tr>\n';
+        all_stacks_display = all_stacks_display + stack_display;
+    };
+    game_board.innerHTML = all_stacks_display;
 };
 
 function get_robot_turn(){
@@ -54,21 +43,26 @@ function get_robot_turn(){
 };
 
 function take_turn(stack, index){
+    log(players[game_state["player_turn"]]["name"] + " took down to " + index + " on stack " + stack);
     game_state["stacks"][stack] = index;
+    if (game_over()){
+        update_game_board(game_state);
+        if (game_state["misere"]){
+            log("Game over! " + players[game_state["player_turn"]]["name"] + " won!!");
+        } else {
+            log("Game over! " + players[game_state["player_turn"]]["name"] + " lost!!");
+        };
+        return;
+    };
     game_state["player_turn"] = game_state["player_turn"] + 1
     if (game_state["player_turn"] == players.length){
         game_state["player_turn"] = 0;
     };
     update_game_board(game_state);
-    if (game_over()){
-        var winner = get_winner();
-        log(winner);
-    };
     if (players[game_state["player_turn"]]["type"] == "Robot"){
         robot_turn = get_robot_turn();
-        sleep(3);
-        take_turn(robot_turn["stack"], robot_turn["index"])
+        setTimeout(take_turn, 2000, robot_turn["stack"], robot_turn["index"])
     }
 };
 
-create_game_board();
+update_game_board();
