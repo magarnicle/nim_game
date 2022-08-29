74dde32ffc2d9be5202198160115a2a29472d68c
Matt Rademaker <matthew@acctv.com.au>
2022-08-30 01:38:45 +1000

    fix(robot turn): Plays randomly from allowed moves
    Knows which stacks are full, picks one of those randomly, picks a random coin from that stack. Also added reset button.

--- game.js
+++ game.js
@@ -1,13 +1,21 @@
 var game_board = document.getElementById("game_board");
 var game_log = document.getElementById("game_log");
-var players = [
-    {"name": "Hugh", "type": "Human"},
-    {"name": "Rob", "type": "Robot"}
-];
-var game_state = {
-    "player_turn": 0,
-    "stacks": [9, 9, 9],
-    "misere": false
+var reset_button = document.getElementById("reset_button")
+var players = [];
+var game_state = {};
+function reset(){
+    players = [
+        {"name": "Hugh", "type": "Human"},
+        {"name": "Rob", "type": "Robot"}
+    ];
+    game_state = {
+        "player_turn": 0,
+        "stacks": [9, 9, 9],
+        "misere": false
+    };
+    reset_button.style = "visibility:hidden";
+    game_log.innerHTML = "Welcome to Nim!"
+    update_game_board();
 };
 
 function log(message){
@@ -39,7 +47,16 @@ function update_game_board(){
 };
 
 function get_robot_turn(){
-    return {"stack": 0, "index": 0};
+    var valid_stacks = [];
+    for (stack in game_state["stacks"]){
+        if (game_state["stacks"][stack] > 0){
+            console.log("stack " + stack + " is valid with " + game_state["stacks"][stack] + " coins")
+            valid_stacks.push(stack)
+        }
+    }
+    stack_choice = valid_stacks[Math.floor(Math.random() * valid_stacks.length)]
+    index_choice = Math.floor(Math.random() * game_state["stacks"][stack_choice])
+    return {"stack": stack_choice, "index": index_choice};
 };
 
 function take_turn(stack, index){
@@ -52,6 +69,7 @@ function take_turn(stack, index){
         } else {
             log("Game over! " + players[game_state["player_turn"]]["name"] + " lost!!");
         };
+        reset_button.style = "visibility:visible";
         return;
     };
     game_state["player_turn"] = game_state["player_turn"] + 1
@@ -61,8 +79,8 @@ function take_turn(stack, index){
     update_game_board(game_state);
     if (players[game_state["player_turn"]]["type"] == "Robot"){
         robot_turn = get_robot_turn();
-        setTimeout(take_turn, 2000, robot_turn["stack"], robot_turn["index"])
+        setTimeout(take_turn, 1000, robot_turn["stack"], robot_turn["index"])
     }
 };
 
-update_game_board();
+reset();
